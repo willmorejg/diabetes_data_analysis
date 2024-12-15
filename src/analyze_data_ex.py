9aa0855ea4bfd1f6edfc2ab48d7a9ebd819fe692
James G Willmore <willmorejg@gmail.com>
2024-12-15 14:33:35 -0500

    Add example analysis example script.

---
+++ src/analyze_data_ex.py
@@ -0,0 +1,25 @@
+# Copyright 2024 James G Willmore
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" Example script to analyze diabetes database data """
+
+import os
+from mods.analyze_diabetes_db_data import AnalyzeDiabetesDbData
+
+diabetes_db = AnalyzeDiabetesDbData()
+df = diabetes_db.read_data_days_from_now(90)
+df_new_ratio = diabetes_db.recalculate_bolus_ratio(df)
+new_ratio_df = df_new_ratio.copy()
+new_bolus_ratio = new_ratio_df[(new_ratio_df.new_ratio > 0)].new_ratio.mean()
+print(f"New every meal bolus ratio: {new_bolus_ratio}")
